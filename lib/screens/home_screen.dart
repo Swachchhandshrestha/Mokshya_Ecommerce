8e9b128909bdd37fc67756b474238a28faa522d8
kishor <kishorkarki53@Gmail.com>
2020-11-20 21:35:00 +0545

    second commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:kishor/components/SearchBar.dart';
+import 'main_drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routename='/home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Center(child: Text('Welcome to Mokshya Store'),),
+      ),
+      drawer: MainDrawer(),
+      body: SafeArea(
+        child:Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 10,bottom: 10),
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius:BorderRadius.circular(10),
+              ),
+              child: Text("slkdfsnf")
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
